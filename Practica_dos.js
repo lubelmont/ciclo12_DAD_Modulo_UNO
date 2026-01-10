285d73b3cd4611008bf4975a4ef505a447f473b5
Arnulfo Rodriguez <luis.rodriguez@lubelsoft.com>
2026-01-10 12:45:37 -0600

    Practica dos

---
+++ Practica_dos.js
@@ -0,0 +1,22 @@
+console.log("\n=== RETO FINAL ===");
+/* TODO: Crea un programa que calcule el promedio de las calicaciones dada un arreglo de
+calificaciones y muestre el resultado en consola donde
+A = 90 - 100
+B = 80 - 89
+C = 70 - 79
+D = 60 - 69
+F = 0 - 59
+
+Arreglos:
+calificacionesUno = [85, 90, 78, 92, 88];
+calificacionesDos = [85, 90, 78];
+calificacionesTres = [65, 70, 72, 68, 60];
+
+*/
+let calificaciones = [65, 70, 72, 68, 60];
+console.log("Calificaciones:", calificaciones);
+let resultado = 0;
+
+
+// TODO: Muestra un mensaje: "Mi promedio es: [resultado]"
+console.log(`Mi promedio es: ${resultado}`);
