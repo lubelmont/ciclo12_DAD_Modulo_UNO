f5c26f442f99567cdfe5b2c1adf2bbcec5d2de91
Arnulfo Rodriguez <luis.rodriguez@lubelsoft.com>
2026-01-10 11:05:31 -0600

    Iniciando if/else

---
+++ condicionales.js
@@ -0,0 +1,39 @@
+
+
+//Ejercicio 1 Condicionales
+console.log("=== EJERCICIO 1: CONDICIONALES ===");
+//Puede votar ? 
+const edadVotante = 17;
+
+if (edadVotante >= 18) {
+    console.log("Puede votar");
+} 
+
+else 
+{
+    console.log("No puede votar");
+}
+
+console.log("=== FIN DEL EJERCICIO 1 ===");
+
+
+//Ejercicio 2 Condicionales
+console.log("\n=== EJERCICIO 2: CONDICIONALES ===");
+let calificacion = 85;
+
+if (calificacion >= 90) {
+    console.log("Excelente");
+} else if (calificacion >= 80) {
+    console.log("Aprobado");
+} else if (calificacion <= 70){
+    
+    console.log("Reprobado");
+} else {
+    console.log("Calificación inválida");
+}
+
+console.log("=== FIN DEL EJERCICIO 2 ===");
+
+
+
+
